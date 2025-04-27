5095e6a4f9259a73c84684bb22c295ab5f151d5b
Josef Caique <jcaique10@gmail.com>
2025-04-27 03:00:58 -0300

    Adding parsing functions

---
+++ src/main/java/com/josef/api_rest/controllers/MathController.java
@@ -0,0 +1,32 @@
+package com.josef.api_rest.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static java.lang.Double.parseDouble;
+
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+    @RequestMapping("/sum/{num1}/{num2}")
+    public Double sum(@PathVariable("num1") String num1,
+                      @PathVariable("num2") String num2)
+    {
+        if (!isNumeric(num1) || !isNumeric(num2)) throw new IllegalArgumentException();
+        return convertToDouble(num1) + convertToDouble(num2);
+    }
+
+    private boolean isNumeric(String strNum){
+        if (strNum == null || strNum.isEmpty()) return false;
+        String number = strNum.replace(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+
+    private Double convertToDouble(String num){
+        if (!isNumeric(num)) throw new IllegalArgumentException();
+        return Double.parseDouble(num);
+    }
+}
