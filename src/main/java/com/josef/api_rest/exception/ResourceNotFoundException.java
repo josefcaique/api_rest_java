b450fc206eab8cf5e038fae54d7b7e0d9ec82dc0
Josef Caique <jcaique10@gmail.com>
2025-04-28 22:31:05 -0300

    changing services with repositories

--- src/main/java/com/josef/api_rest/exception/ResourceNotFoundException.java
+++ src/main/java/com/josef/api_rest/exception/ResourceNotFoundException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class UnsupportedMathException extends RuntimeException {
+public class ResourceNotFoundException extends RuntimeException {
 
-    public UnsupportedMathException(String message) {
+    public ResourceNotFoundException(String message) {
 
         super(message);
     }
