fbfd1316d17b8d5b8c670f3a9b54313f05c5f997
Josef Caique <jcaique10@gmail.com>
2025-05-02 14:33:22 -0300

    adding tests

--- src/main/java/com/josef/api_rest/mapper/ObjectMapper.java
+++ src/main/java/com/josef/api_rest/mapper/ObjectMapper.java
@@ -14,7 +14,7 @@ public class ObjectMapper {
         return mapper.map(origin, destination);
     }
 
-    public static <O, D> List<D> parseListObject(List<O> origin, Class<D> destination) {
+    public static <O, D> List<D> parseListObjects(List<O> origin, Class<D> destination) {
         List<D> destinationObjects = new ArrayList<D>();
         for (Object o: origin) {
             destinationObjects.add(mapper.map(o, destination));
