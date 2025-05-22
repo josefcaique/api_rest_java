006ed50d3e7f8ee43ccc47da86c3df02d29d4947
Josef Caique <jcaique10@gmail.com>
2025-05-21 22:44:19 -0300

    Creating class Book

---
+++ src/main/java/com/josef/api_rest/model/Book.java
@@ -0,0 +1,76 @@
+package com.josef.api_rest.model;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Book {
+
+    private Long Id;
+    private String author;
+    private Date launchDate;
+    private Double price;
+    private String title;
+
+    public Book(){}
+
+    public Book(Long id, String author, Date launchDate, Double price, String title) {
+        Id = id;
+        this.author = author;
+        this.launchDate = launchDate;
+        this.price = price;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getLaunchDate() {
+        return launchDate;
+    }
+
+    public void setLaunchDate(Date launchDate) {
+        this.launchDate = launchDate;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(Id, book.Id) && Objects.equals(author, book.author) && Objects.equals(launchDate, book.launchDate) && Objects.equals(price, book.price) && Objects.equals(title, book.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Id, author, launchDate, price, title);
+    }
+}
