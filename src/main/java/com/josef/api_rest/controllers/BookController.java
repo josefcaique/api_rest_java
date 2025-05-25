a2515495c2336ac7a91d1905f9cc61368e993ff6
Josef Caique <jcaique10@gmail.com>
2025-05-25 16:23:43 -0300

    addig consumes and produces to xml in controller

--- src/main/java/com/josef/api_rest/controllers/BookController.java
+++ src/main/java/com/josef/api_rest/controllers/BookController.java
@@ -4,6 +4,7 @@ import com.josef.api_rest.data.dto.v1.BookDTO;
 import com.josef.api_rest.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,27 +17,27 @@ public class BookController {
     @Autowired
     private BookService service;
 
-    @GetMapping(value = "/v1/{id}")
+    @GetMapping(value = "/v1/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public BookDTO findById(@PathVariable Long id) {
         return service.findById(id);
     }
 
-    @GetMapping(value = "/v1")
+    @GetMapping(value = "/v1", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public List<BookDTO> findAll(){
         return service.findAll();
     }
 
-    @PostMapping(value = "/v1")
+    @PostMapping(value = "/v1", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public BookDTO create(@RequestBody BookDTO bookDTO) {
         return service.create(bookDTO);
     }
 
-    @PutMapping(value = "/v1")
+    @PutMapping(value = "/v1",consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public BookDTO update(@RequestBody BookDTO bookDTO) {
         return service.update(bookDTO);
     }
 
-    @DeleteMapping(value = "/v1/{id}")
+    @DeleteMapping(value = "/v1/{id}",consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<?> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
