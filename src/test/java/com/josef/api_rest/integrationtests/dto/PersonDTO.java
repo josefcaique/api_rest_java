a6c348b3b2f107a51bd52141d7d8bbfc1978e641
Josef Caique <jcaique10@gmail.com>
2025-06-10 23:03:58 -0300

    creating person dto to test

---
+++ src/test/java/com/josef/api_rest/integrationtests/dto/PersonDTO.java
@@ -0,0 +1,120 @@
+package com.josef.api_rest.integrationtests.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.josef.api_rest.serializer.GenderSerializer;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class PersonDTO implements Serializable {
+
+    private static final long SerialVersionUID = 1L;
+
+    private Long id;
+
+    // @JsonProperty("first_name")
+    private String firstName;
+
+    // @JsonProperty("last_name")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String lastName;
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private String phoneNumber;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private Date birthDay;
+    private String address;
+
+    @JsonSerialize(using = GenderSerializer.class)
+    private String gender;
+
+    private String sensitiveData;
+
+    public PersonDTO(){}
+
+    public PersonDTO(Long id, String firstName, String lastName, String address, String gender) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.gender = gender;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthDay() { return birthDay;}
+
+    public void setBirthDay(Date birthDay) { this.birthDay = birthDay; }
+
+    public String getPhoneNumber() { return phoneNumber; }
+
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+
+    public String getSensitiveData() {
+        return sensitiveData;
+    }
+
+    public void setSensitiveData(String sensitiveData) {
+        this.sensitiveData = sensitiveData;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonDTO person = (PersonDTO) o;
+        return Objects.equals(id, person.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
