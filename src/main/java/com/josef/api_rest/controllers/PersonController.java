e3487e143fb12873f6fb9145465bddd9f1f5b337
Josef Caique <jcaique10@gmail.com>
2025-06-17 00:10:34 -0300

    fixing direction

--- src/main/java/com/josef/api_rest/controllers/PersonController.java
+++ src/main/java/com/josef/api_rest/controllers/PersonController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -36,7 +37,7 @@ public class PersonController implements PersonControllerDocs {
             @RequestParam(value="direction", defaultValue="asc") String direction
     ){
         var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
-        Pageable pageable = PageRequest.of(page, size, sortDirection);
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "firstName"));
         return ResponseEntity.ok(services.findAll(pageable));
     }
 
