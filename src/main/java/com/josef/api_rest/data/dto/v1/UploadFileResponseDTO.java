6acce4bd8c916e760aee7085786e12f4bfafd624
Josef Caique <jcaique10@gmail.com>
2025-06-25 23:55:43 -0300

    adding upload file in FileStorageController

--- src/main/java/com/josef/api_rest/data/dto/v1/UploadFileResponseDTO.java
+++ src/main/java/com/josef/api_rest/data/dto/v1/UploadFileResponseDTO.java
@@ -10,11 +10,11 @@ public class UploadFileResponseDTO implements Serializable {
     private String fileName;
     private String fileDownloadUri;
     private String fileType;
-    private String size;
+    private Long size;
 
     public UploadFileResponseDTO() {}
 
-    public UploadFileResponseDTO(String fileName, String fileDownloadUri, String fileType, String size) {
+    public UploadFileResponseDTO(String fileName, String fileDownloadUri, String fileType, Long size) {
         this.fileName = fileName;
         this.fileDownloadUri = fileDownloadUri;
         this.fileType = fileType;
@@ -45,11 +45,11 @@ public class UploadFileResponseDTO implements Serializable {
         this.fileType = fileType;
     }
 
-    public String getSize() {
+    public Long getSize() {
         return size;
     }
 
-    public void setSize(String size) {
+    public void setSize(Long size) {
         this.size = size;
     }
 
