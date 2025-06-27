ba9c80eb9b551fd0351ae90fc733829fbf0aa258
Josef Caique <jcaique10@gmail.com>
2025-06-27 19:43:27 -0300

    adding download file

--- src/main/java/com/josef/api_rest/controllers/docs/FileStorageControllerDocs.java
+++ src/main/java/com/josef/api_rest/controllers/docs/FileStorageControllerDocs.java
@@ -3,6 +3,7 @@ package com.josef.api_rest.controllers.docs;
 import com.josef.api_rest.data.dto.v1.UploadFileResponseDTO;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,5 +14,5 @@ public interface FileStorageControllerDocs {
 
     UploadFileResponseDTO uploadFile(MultipartFile file);
     List<UploadFileResponseDTO> uploadMultipleFiles(MultipartFile[] files);
-    ResponseEntity<ResponseEntity> downloadFile(String fileName, HttpServletRequest request);
+    ResponseEntity<Resource> downloadFile(String fileName, HttpServletRequest request);
 }
