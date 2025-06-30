f1edaeddf05cb69e0ad424bd1f712323d5aedb3a
Josef Caique <jcaique10@gmail.com>
2025-06-29 23:40:27 -0300

    adding data creation in docs

--- src/main/java/com/josef/api_rest/controllers/docs/PersonControllerDocs.java
+++ src/main/java/com/josef/api_rest/controllers/docs/PersonControllerDocs.java
@@ -10,6 +10,9 @@ import org.springframework.hateoas.PagedModel;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface PersonControllerDocs {
 
@@ -48,6 +51,22 @@ public interface PersonControllerDocs {
             @RequestParam(value="size", defaultValue ="12") Integer size,
             @RequestParam(value="direction", defaultValue = "asc") String direction
     );
+
+    @Operation(summary = "Insert people through data file",
+            description = "Insert many people as need through a file as .csv ou .xlsx",
+            tags = {"People"},
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = PersonDTO.class))),
+                    @ApiResponse(description = "No content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            })
+    List<PersonDTO> dataCreation(MultipartFile file);
 
 
     @Operation(summary = "Find people by firstName",
