7761bc5f5b3f57d2506c59e754a72285b61c6488
Josef Caique <jcaique10@gmail.com>
2025-07-02 23:42:23 -0300

    finishing EmailSender class

--- src/main/java/com/josef/api_rest/mail/EmailSender.java
+++ src/main/java/com/josef/api_rest/mail/EmailSender.java
@@ -1,8 +1,14 @@
 package com.josef.api_rest.mail;
 
+import com.josef.api_rest.config.EmailConfig;
+import jakarta.mail.MessagingException;
 import jakarta.mail.internet.AddressException;
 import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
@@ -12,6 +18,8 @@ import java.util.StringTokenizer;
 
 @Component
 public class EmailSender implements Serializable {
+
+    Logger logger = LoggerFactory.getLogger(EmailSender.class);
 
     private final JavaMailSender mailSender;
     private String to;
@@ -31,20 +39,52 @@ public class EmailSender implements Serializable {
     }
 
 
-    public EmailSender setSubject(String subject) {
+    public EmailSender withSubject(String subject) {
         this.subject = subject;
+        return this;
     }
 
-    public void setBody(String body) {
+    public EmailSender withMessage(String body) {
         this.body = body;
+        return this;
     }
 
     public void setRecipients(ArrayList<InternetAddress> recipients) {
         this.recipients = recipients;
     }
 
-    public void setAttachment(File attachment) {
-        this.attachment = attachment;
+    public EmailSender attach(File fileDir) {
+        this.attachment = new File(String.valueOf(fileDir));
+        return this;
+    }
+
+    public void send(EmailConfig config) {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = null;
+        try {
+            helper = new MimeMessageHelper(message, true);
+            helper.setFrom(config.getUsername());
+            helper.setTo(recipients.toArray(new InternetAddress[0]));
+            helper.setSubject(subject);
+            helper.setText(body, true);
+            if (attachment != null) {
+                helper.addAttachment(attachment.getName(), attachment);
+            }
+            mailSender.send(message);
+            logger.info("email sent to %s with the subject '%s'%n", to, subject);
+            reset();
+        } catch (MessagingException e) {
+            throw new RuntimeException("Error sending the email",e);
+        }
+
+    }
+
+    private void reset() {
+        this.to = null;
+        this.subject = null;
+        this.body = null;
+        this.recipients = null;
+        this.attachment = null;
     }
 
     private static ArrayList<InternetAddress> getRecipients(String to) {
