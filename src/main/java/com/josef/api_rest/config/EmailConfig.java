ab5b8a07d05bce0a851735a6f2e67578dc9a2ee8
Josef Caique <jcaique10@gmail.com>
2025-07-02 23:03:45 -0300

    creating EmailConfig

---
+++ src/main/java/com/josef/api_rest/config/EmailConfig.java
@@ -0,0 +1,67 @@
+package com.josef.api_rest.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.mail")
+public class EmailConfig {
+
+    private String host;
+    private int port;
+    private String username;
+    private String password;
+    private String from;
+    private boolean ssl;
+
+
+    public EmailConfig() {}
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public boolean isSsl() {
+        return ssl;
+    }
+
+    public void setSsl(boolean ssl) {
+        this.ssl = ssl;
+    }
+}
