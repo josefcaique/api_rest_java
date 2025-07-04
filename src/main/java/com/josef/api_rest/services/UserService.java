58f783c319ff8bcbce66b7c5b0a5c85416df4721
Josef Caique <jcaique10@gmail.com>
2025-07-04 15:11:21 -0300

    developing UserService

---
+++ src/main/java/com/josef/api_rest/services/UserService.java
@@ -0,0 +1,24 @@
+package com.josef.api_rest.services;
+
+import com.josef.api_rest.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = repository.findUserByName(username);
+        if (user != null) return user;
+        throw new UsernameNotFoundException("Username "+username+ " not found!");
+    }
+}
