c21de007d1e0f012cd235d800ac24916686f3f1e
Josef Caique <jcaique10@gmail.com>
2025-07-06 16:14:24 -0300

    creating controller to Auth

---
+++ src/main/java/com/josef/api_rest/controllers/AuthController.java
@@ -0,0 +1,39 @@
+package com.josef.api_rest.controllers;
+
+import com.josef.api_rest.data.dto.v1.security.AccountCredentialsDTO;
+import com.josef.api_rest.data.dto.v1.security.TokenDTO;
+import com.josef.api_rest.services.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name="authentication endpoint")
+@RestController
+@RequestMapping("/auth" )
+public class AuthController {
+
+    @Autowired
+    AuthService service;
+
+    @Operation(summary = "Authenticates an user and returns a token")
+    @PostMapping("/signin")
+    public ResponseEntity<?> signIn(AccountCredentialsDTO credentials) {
+        if (credentialsIsInvalid(credentials)) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
+
+        var token = service.signIn(credentials);
+
+        if (token == null) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
+        return ResponseEntity.ok().body(token);
+    }
+
+    private static boolean credentialsIsInvalid(AccountCredentialsDTO credentials) {
+        return credentials == null ||
+                StringUtils.isBlank(credentials.getPassword()) ||
+                StringUtils.isBlank(credentials.getUsername());
+    }
+}
