e88f65a321b0d74d27ab44e6f8f6b8c0b72ff2cb
Josef Caique <jcaique10@gmail.com>
2025-07-07 22:51:48 -0300

    test signin in AuthControllerTest

---
+++ src/test/java/com/josef/api_rest/integrationtests/controllers/withjson/AuthControllerTest.java
@@ -0,0 +1,50 @@
+package com.josef.api_rest.integrationtests.controllers.withjson;
+import com.josef.api_rest.config.TestConfigs;
+import com.josef.api_rest.integrationtests.dto.AccountCredentialsDTO;
+import com.josef.api_rest.integrationtests.dto.TokenDTO;
+import com.josef.api_rest.integrationtests.testcontainers.AbstractIntegrationTest;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+
+import static io.restassured.RestAssured.given;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class AuthControllerTest extends AbstractIntegrationTest {
+
+    private static TokenDTO tokenDto;
+
+    @BeforeAll
+    static void setUp() {
+        tokenDto = new TokenDTO();
+    }
+
+    @Test
+    @Order(1)
+    void signin() {
+        AccountCredentialsDTO credentials = AccountCredentialsDTO.createAccountCredentialsDTO("leandro", "admin123");
+        tokenDto = given()
+                .basePath("/auth/signin")
+                .port(TestConfigs.SERVER_PORT)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(credentials)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .as(TokenDTO.class);
+
+        Assertions.assertNotNull(tokenDto.getAccessToken());
+        Assertions.assertNotNull(tokenDto.getRefreshToken());
+    }
+
+    @Test
+    @Order(2)
+    void refreshToken() {
+
+    }
+}
---
+++ src/test/java/com/josef/api_rest/integrationtests/dto/BookDTO.java
@@ -0,0 +1,94 @@
+package com.josef.api_rest.integrationtests.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "Book")
+@Relation(collectionRelation="Book")
+public class BookDTO extends RepresentationModel<BookDTO> implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String author;
+
+    @JsonFormat(pattern = "dd/mm/yyyy")
+    private Date launchDate;
+    private Double price;
+    private String title;
+
+    public BookDTO(){}
+
+    public BookDTO(Long id, String author, Date launchDate, Double price, String title) {
+        this.id = id;
+        this.author = author;
+        this.launchDate = launchDate;
+        this.price = price;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getLaunchDate() {
+        return launchDate;
+    }
+
+    public void setLaunchDate(Date launchDate) {
+        this.launchDate = launchDate;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        BookDTO bookDTO = (BookDTO) o;
+        return Objects.equals(id, bookDTO.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id);
+    }
+}
